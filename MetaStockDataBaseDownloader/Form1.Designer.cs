ab82c1e1344693917a1a20df1d812e204d396197
Luis Barral <barven2013@gmail.com>
2018-02-03 15:54:44 -0300

    Add download function Add delete folder function Add delete file function

--- MetaStockDataBaseDownloader/Form1.Designer.cs
+++ MetaStockDataBaseDownloader/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 218);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(471, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Donwload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(447, 134);
+            this.listBox1.TabIndex = 2;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(389, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(471, 240);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Metastock database Downloader";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- MetaStockDataBaseDownloader/Form1.cs
+++ MetaStockDataBaseDownloader/Form1.cs
@@ -1,20 +1,138 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
 
 namespace MetaStockDataBaseDownloader
 {
     public partial class Form1 : Form
     {
+
+        public string filename { get; set; }
+
+        WebClient client;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            DeleteFolderContent();
+            DownloadFile();
+        }
+
+        private void DownloadFile()
+        {
+
+            client = new WebClient();
+
+            listBox1.Items.Add(string.Format("Download start at {0}", DateTime.Now));
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            string desktopPath = @"C:\MetaStock Data\Acciones";
+
+            string url = "http://resources.invertironline.com/iolresearch/IOL_Metastock.zip";
+
+            filename = GetFilename(url);
+
+            using (client)
+            {
+                client.DownloadProgressChanged += Wc_DownloadProgressChanged;
+                client.DownloadFileCompleted += Wc_DownloadFileCompleted;
+                client.DownloadFileAsync(new Uri(url), desktopPath + "/" + filename);
+            }
+        }
+
+        private string GetFilename(string hreflink)
+        {
+            Uri uri = new Uri(hreflink);
+
+            string filename = System.IO.Path.GetFileName(uri.LocalPath);
+
+            return filename;
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            toolStripProgressBar1.Value = e.ProgressPercentage;
+            toolStripStatusLabel1.Text = e.ProgressPercentage + "% | " + e.BytesReceived + " bytes out of " +
+                e.TotalBytesToReceive + " bytes retrieven.";
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            toolStripProgressBar1.Value = 0;
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+
+            if (e.Cancelled)
+            {
+                listBox1.Items.Add("The download has been cancelled");
+
+                return;
+            }
+
+            if (e.Error != null) // We have an error! Retry a few times, then abort.
+            {
+                listBox1.Items.Add("An error ocurred while trying to download file");
+
+                return;
+            }
+
+            listBox1.Items.Add(string.Format("Download completed at {0}", DateTime.Now));
+
+            UnZipFile();
+
+        }
+
+        private void CancelDownload()
+        {
+            client.CancelAsync();
+        }
+
+        private void UnZipFile()
+        {
+            listBox1.Items.Add(string.Format("Unzipping downloaded file {0}", filename));
+
+            string zipPath = @"C:\MetaStock Data\Acciones\IOL_Metastock.zip";
+            string extractPath = @"C:\MetaStock Data\Acciones";
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+            listBox1.Items.Add(string.Format("Unzipping completed"));
+
+            listBox1.Items.Add(string.Format("Deleting downloaded file"));
+
+            DeleteFile();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            CancelDownload();
+        }
+
+        private void DeleteFolderContent()
+        {
+            listBox1.Items.Add(string.Format("Deleting folder content"));
+            Array.ForEach(Directory.GetFiles(@"C:\MetaStock Data\Acciones"), File.Delete);
+        }
+
+        private void DeleteFile()
+        {
+            listBox1.Items.Add(string.Format("Deleting file {0}", filename));
+            File.Delete(string.Format(@"C:\MetaStock Data\Acciones\{0}", filename));
+        }
+
     }
 }
